de0274dfaeff0ea10cd73af9e6430f5fcbb26c58
AR-Shahin <mdshahinmije96@gmail.com>
2021-11-24 21:02:20 +0600

    hollow half pyramid pattrn solve

--- basic/pattern.py
+++ basic/pattern.py
@@ -1,6 +1,11 @@
 n = 5
 
-for i in range(n, 0, -1):
-    for j in range(i, 0, -1):
-        print(j, end=" ")
+for i in range(1, n+1):
+    sum = 0
+    for j in range(1, i+1):
+        sum += 1
+        if j < 2 or i == sum or i == n:
+            print(j, end=" ")
+        else:
+            print(" ", end=" ")
     print()
