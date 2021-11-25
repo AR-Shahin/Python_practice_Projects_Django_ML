b130758d732cb79e35c8f9383b39f3561096f28d
AR-Shahin <mdshahinmije96@gmail.com>
2021-11-25 18:46:41 +0600

    implement selection sort

---
+++ DSA/section_sort.py
@@ -0,0 +1,26 @@
+
+# Selection Sort
+def selection_sort(arr, mode='asc'):
+    l = len(arr)
+    for i in range(0, l):
+        min_index = i
+        for j in range(i+1, l):
+            if mode == 'asc':
+                if arr[min_index] > arr[j]:
+                    min_index = j
+            elif mode == 'dsc':
+                if arr[min_index] < arr[j]:
+                    min_index = j
+
+        if i != min_index:
+            temp = arr[i]
+            arr[i] = arr[min_index]
+            arr[min_index] = temp
+    print(arr)
+
+
+arr = [10, 10, 15, 16, 45, 9, 2, 1, 50]
+
+selection_sort(arr, 'dsc')
+
+# print(arr)
